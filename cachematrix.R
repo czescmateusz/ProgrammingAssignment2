67f6bd75369f9cf69789902a174716e35dfb5d57
Mateusz Pawłowski <mateusz.piotr.pawlowski@gmail.com>
2016-04-09 19:00:17 +0200

    Commiting a ProgrammingAssigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#makeCachematrix provides a list with a function returning the value of a the matrix(set), 
+#getting the value(via get), sets the inverse of a matrix, and gets the inverse of a matrix
+# makeCacheMatrix creates a list containing a function to
+#I'm also using the  ginv function from the MASS package to compute the inverse of non-square matrixes
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  require(MASS)
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(ginv) inv <<- ginv
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+# The function returns the inverse of a matrix via a ginv function from the MASS package.
+# If the inverse was already calcuated it returns the value and skips the computation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  require(MASS)
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- ginv(data)
+  x$setinverse(inv)
+  inv
 }
+
+#Sample of function
+matrix(1:6, 2, 3)
+m <- makeCacheMatrix(matrix(1:6, 2, 3))
+cacheSolve(m)
+
+
+
+
+
+
